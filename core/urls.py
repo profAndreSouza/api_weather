389564f0c40ca84e688e31dc1ada6e45411f9f33
Andre <andre.souza@sp.senai.br>
2024-03-21 22:45:38 -0300

    feat:add form to manual input of climate data
    add forms with data fields, created route to manual insertion of data climate and add in template main hyperref to this route
    
    only me

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from weather.views import WeatherView, WeatherGenerate, WeatherReset
+from weather.views import WeatherView, WeatherGenerate, WeatherReset, WeatherInsert
 
 urlpatterns = [
     path('', WeatherView.as_view(), name='Weather View'),
+    path('insert', WeatherInsert.as_view(), name='Weather Insert'),
     path('generate', WeatherGenerate.as_view(), name='Weather Generate'),
     path('reset', WeatherReset.as_view(), name='Weather Reset'),
 ]
--- weather/views.py
+++ weather/views.py
@@ -6,6 +6,7 @@ from django.shortcuts import render, redirect
 from .models import WeatherEntity
 from .repositories import WeatherRepository
 from .serializers import WeatherSerializer
+from .forms import WeatherForm
 
 class WeatherView(View):
     def get(self, request):
@@ -41,5 +42,25 @@ class WeatherReset(View):
     def get(self, request): 
         repository = WeatherRepository(collectionName='weathers')
         repository.deleteAll()
+
+        return redirect('Weather View')
+    
+class WeatherInsert(View):
+    def get(self, request):
+        weatherForm = WeatherForm()
+
+        return render(request, "form.html", {"form":weatherForm})
+    
+    def post(self, request):
+        weatherForm = WeatherForm(request.POST)
+        if weatherForm.is_valid():
+            serializer = WeatherSerializer(data=weatherForm.data)
+            if (serializer.is_valid()):
+                repository = WeatherRepository(collectionName='weathers')
+                repository.insert(serializer.data)
+            else:
+                print(serializer.errors)
+        else:
+            print(weatherForm.errors)
 
         return redirect('Weather View')
